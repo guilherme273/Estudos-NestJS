aab2790c64dc12e75825490d76532906114fb48b
GUILHERME FEITOSA CUNHA <126368583+guilherme273@users.noreply.github.com>
2025-03-27 20:53:06 -0300

    Adicionando bcripyt a senha no post do user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma.service';
 import { User, Prisma } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
   async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    const hash = await bcrypt.hash(data.password, 10);
     return this.prisma.user.create({
-      data,
+      data: { ...data, password: hash },
     });
   }
 }
